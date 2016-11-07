654c19b231234b7635b5528526ff403532781f7d
linyxy <linyxy.art@gmail.com>
2016-11-07 00:04:00 -0800

    merge initial code

---
+++ README.md
@@ -0,0 +1,20 @@
+# CS 184 Fall 2016
+Instructor: James O'Brien
+
+# USAGE
+‘cd’ into your assignment directory, type into the Terminal the following step by step
+
+1. mkdir build
+2. cd build
+3. cmake ..
+4. make
+
+# Keyboard features
+1. 'ESC': Exit
+2. 'Q': Exit
+3. 'F': Full screen
+4. 'Shift + ↓': Translate objects down
+5. 'Shift + ↑': Translate objects up
+6. 'Shift + ←': Translate objects left
+7. 'Shift + →': Translate objects right
+8. Write your own to match the spec...
---
+++ src/example_03.cpp
@@ -0,0 +1,237 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+//include header file for glfw library so that we can use OpenGL
+#include <GLFW/glfw3.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#ifdef _WIN32
+static DWORD lastTime;
+#else
+static struct timeval lastTime;
+#endif
+
+#define PI 3.14159265 // Should be used from mathlib
+
+using namespace std;
+
+/*
+For UC Berkeley's CS184 Fall 2016 course, assignment 3 (Bezier surfaces)
+*/
+
+//****************************************************
+// Global Variables
+//****************************************************
+GLfloat translation[3] = {0.0f, 0.0f, 0.0f};
+bool auto_strech = false;
+int Width_global = 400;
+int Height_global = 400;
+int Z_buffer_bit_depth = 128;
+
+inline float sqr(float x) { return x*x; }
+
+
+//****************************************************
+// Simple init function
+//****************************************************
+void initializeRendering()
+{
+    glfwInit();
+}
+
+
+//****************************************************
+// A routine to set a pixel by drawing a GL point.  This is not a
+// general purpose routine as it assumes a lot of stuff specific to
+// this example.
+//****************************************************
+void setPixel(float x, float y, GLfloat r, GLfloat g, GLfloat b) {
+    glColor3f(r, g, b);
+    glVertex2f(x+0.5, y+0.5);  // The 0.5 is to target pixel centers
+    // Note: Need to check for gap bug on inst machines.
+}
+
+//****************************************************
+// Keyboard inputs. Add things to match the spec! 
+//****************************************************
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    switch (key) {
+            
+        case GLFW_KEY_ESCAPE: glfwSetWindowShouldClose(window, GLFW_TRUE); break;
+        case GLFW_KEY_Q: glfwSetWindowShouldClose(window, GLFW_TRUE); break;
+        case GLFW_KEY_LEFT :
+            if (action && mods == GLFW_MOD_SHIFT) translation[0] -= 0.001f * Width_global; break;
+        case GLFW_KEY_RIGHT:
+            if (action && mods == GLFW_MOD_SHIFT) translation[0] += 0.001f * Width_global; break;
+        case GLFW_KEY_UP   :
+            if (action && mods == GLFW_MOD_SHIFT) translation[1] += 0.001f * Height_global; break;
+        case GLFW_KEY_DOWN :
+            if (action && mods == GLFW_MOD_SHIFT) translation[1] -= 0.001f * Height_global; break;
+        case GLFW_KEY_F:
+            if (action && mods == GLFW_MOD_SHIFT) auto_strech = !auto_strech; break;
+        case GLFW_KEY_SPACE: break;
+            
+        default: break;
+    }
+    
+}
+
+//****************************************************
+// Draw a cube. You don't need this for your final assignment, but it's
+// here so you don't look at a blank screen.
+// Taken from https://www.ntu.edu.sg/home/ehchua/programming/opengl/CG_Examples.html
+//****************************************************
+void drawCube() {
+	 glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
+      // Top face (y = 1.0f)
+      // Define vertices in counter-clockwise (CCW) order with normal pointing out
+      glColor3f(0.0f, 1.0f, 0.0f);     // Green
+      glVertex3f( 1.0f, 1.0f, -1.0f);
+      glVertex3f(-1.0f, 1.0f, -1.0f);
+      glVertex3f(-1.0f, 1.0f,  1.0f);
+      glVertex3f( 1.0f, 1.0f,  1.0f);
+ 
+      // Bottom face (y = -1.0f)
+      glColor3f(1.0f, 0.5f, 0.0f);     // Orange
+      glVertex3f( 1.0f, -1.0f,  1.0f);
+      glVertex3f(-1.0f, -1.0f,  1.0f);
+      glVertex3f(-1.0f, -1.0f, -1.0f);
+      glVertex3f( 1.0f, -1.0f, -1.0f);
+ 
+      // Front face  (z = 1.0f)
+      glColor3f(1.0f, 0.0f, 0.0f);     // Red
+      glVertex3f( 1.0f,  1.0f, 1.0f);
+      glVertex3f(-1.0f,  1.0f, 1.0f);
+      glVertex3f(-1.0f, -1.0f, 1.0f);
+      glVertex3f( 1.0f, -1.0f, 1.0f);
+ 
+      // Back face (z = -1.0f)
+      glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
+      glVertex3f( 1.0f, -1.0f, -1.0f);
+      glVertex3f(-1.0f, -1.0f, -1.0f);
+      glVertex3f(-1.0f,  1.0f, -1.0f);
+      glVertex3f( 1.0f,  1.0f, -1.0f);
+ 
+      // Left face (x = -1.0f)
+      glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+      glVertex3f(-1.0f,  1.0f,  1.0f);
+      glVertex3f(-1.0f,  1.0f, -1.0f);
+      glVertex3f(-1.0f, -1.0f, -1.0f);
+      glVertex3f(-1.0f, -1.0f,  1.0f);
+ 
+      // Right face (x = 1.0f)
+      glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
+      glVertex3f(1.0f,  1.0f, -1.0f);
+      glVertex3f(1.0f,  1.0f,  1.0f);
+      glVertex3f(1.0f, -1.0f,  1.0f);
+      glVertex3f(1.0f, -1.0f, -1.0f);
+   glEnd();  // End of drawing color-cube
+}
+
+
+//****************************************************
+// function that does the actual drawing of stuff
+//***************************************************
+void display( GLFWwindow* window )
+{
+    glClearColor( 0.0f, 0.0f, 0.0f, 0.0f ); //clear background screen to black
+    
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);                // clear the color buffer (sets everything to black)
+    glMatrixMode(GL_MODELVIEW);                  // indicate we are specifying camera transformations
+    glLoadIdentity();                            // make sure transformation is "zero'd"
+    
+    //----------------------- code to draw objects --------------------------
+    glPushMatrix();
+    glTranslatef (translation[0], translation[1], translation[2]);
+    glRotatef(45, 1, 1, 0); //rotates the cube below
+    drawCube(); // REPLACE ME!
+
+    glPopMatrix();
+    
+    glfwSwapBuffers(window);
+
+    // note: check out glPolygonMode and glShadeModel 
+    // for wireframe and shading commands
+    
+}
+
+//****************************************************
+// function that is called when window is resized
+//***************************************************
+void size_callback(GLFWwindow* window, int width, int height)
+{
+    // Get the pixel coordinate of the window
+    // it returns the size, in pixels, of the framebuffer of the specified window
+    glfwGetFramebufferSize(window, &Width_global, &Height_global);
+    
+    glViewport(0, 0, Width_global, Height_global);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, Width_global, 0, Height_global, 1, -1);
+    
+    display(window);
+}
+
+
+//****************************************************
+// the usual stuff, nothing exciting here
+//****************************************************
+int main(int argc, char *argv[]) {
+    //This initializes glfw
+    initializeRendering();
+    
+    GLFWwindow* window = glfwCreateWindow( Width_global, Height_global, "CS184", NULL, NULL );
+    if ( !window )
+    {
+        cerr << "Error on window creating" << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    const GLFWvidmode * mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+    if ( !mode )
+    {
+        cerr << "Error on getting monitor" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent( window );
+    
+    // Get the pixel coordinate of the window
+    // it returns the size, in pixels, of the framebuffer of the specified window
+    glfwGetFramebufferSize(window, &Width_global, &Height_global);
+    
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+	glOrtho(-3.5, 3.5, -3.5, 3.5, 5, -5);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+    glEnable(GL_DEPTH_TEST);	// enable z-buffering
+    glDepthFunc(GL_LESS);
+
+    glfwSetWindowTitle(window, "CS184");
+    glfwSetWindowSizeCallback(window, size_callback);
+    glfwSetKeyCallback(window, key_callback);
+
+    while( !glfwWindowShouldClose( window ) ) // infinite loop to draw object again and again
+    {   // because once object is draw then window is terminated
+        display( window );
+        
+        if (auto_strech){
+            glfwSetWindowSize(window, mode->width, mode->height);
+            glfwSetWindowPos(window, 0, 0);
+        }
+        
+        glfwPollEvents();
+        
+    }
+
+    return 0;
+}
